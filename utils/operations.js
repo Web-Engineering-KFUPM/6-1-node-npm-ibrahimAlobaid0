0b2d3b8798b88274d99c067cca60aa71b41cc059
ibrahimAloabid0 <s202253180@kfupm.edu.sa>
2025-11-11 20:09:19 +0300

    task 4

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,19 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((acc, num) => acc + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  if (numbers.length === 0) return 0;
+  else return numbers.slice(1).reduce((acc, num) => acc - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  if (numbers.length === 0) return 0;
+  else return numbers.reduce((acc, num) => acc * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  if (numbers.length === 0) return 0;
+  else return numbers.slice(1).reduce((acc, num) => acc / num, numbers[0]);
 }
 
