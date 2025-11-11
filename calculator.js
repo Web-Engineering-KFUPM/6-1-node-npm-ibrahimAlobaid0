8d0440f32014d1cbcde5a6456c556a643f2b3794
ibrahimAloabid0 <s202253180@kfupm.edu.sa>
2025-11-11 20:03:15 +0300

    task 2

--- calculator.js
+++ calculator.js
@@ -178,3 +178,5 @@ import { add, subtract, multiply, divide } from "./utils/operations.js";
 import { parseNumbers, isValidOperation } from "./utils/parser.js";
 import _ from "lodash";
 
+const operation = process.argv[2];
+const numbers = process.argv.slice(3);
